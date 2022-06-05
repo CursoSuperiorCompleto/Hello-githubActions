350ed05b8d9076f2656fb22aa7f9d3df338863b2
felipeleonardo98 <felipeferrazleonardo98@gmail.com>
2022-06-05 20:45:40 -0300

    new features

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p> Hey, there! What's up? </p>"
 
+@app.route("/main")
+def who_am_i():
+    return "<p> I AM A FLASK APPLICATION! </p>"
+
 @app.route("/users")
 def get_users():
     return "<h1> Imagine a json returning right here! </h1>"
