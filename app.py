b7ffc62fa1c43964a4c1909bffcb3c54f9f19f1d
felipeleonardo98 <felipeferrazleonardo98@gmail.com>
2022-06-05 22:38:20 -0300

    vamos ver

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return "<p> Hey, there! What's up? </p>"
 
+@app.route("/application")
+def who_am_i():
+    return "<h1> I AM A FLASK APPLICATION! </h1>"
+
 @app.route("/users")
 def get_users():
     return "<h1> Imagine a json returning right here! </h1>"
