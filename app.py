d50ab2095d7049c2f3f963f3102b6401f499aa14
felipeleonardo98 <felipeferrazleonardo98@gmail.com>
2022-06-05 21:19:07 -0300

    python formatter review

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p> Hey, there! What's up? </p>"
 
+
 @app.route("/main")
 def who_am_i():
     return "<p> I AM A FLASK APPLICATION! </p>"
+
 
 @app.route("/users")
 def get_users():
