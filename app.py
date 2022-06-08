748ff84a3d612f034645344b6628b8f9420b03b8
felipeleonardo98 <felipeferrazleonardo98@gmail.com>
2022-06-08 14:48:24 -0300

    lint test 1

--- app.py
+++ app.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/docker")
 def hello_world():
     return "<p> HEY, MY NAME IS DOCKER! </p>"
 
+
 @app.route("/python")
 def who_am_i():
     return "<h1> HEEEEEEEEEEEEEEEEEEEEEY, DO YOU ALREADY KNOW ME?? </h1>"
+
 
 @app.route("/spark")
 def get_users():
