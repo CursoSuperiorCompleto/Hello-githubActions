aa00733493600926e081d33e6416a94e88c909f2
FelipeLeonardo98 <felipeferrazleonardo98@gmail.com>
2023-03-05 14:04:46 -0300

    feat: BIX chapter data engineering context

--- app.py
+++ app.py
@@ -2,14 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/docker")
+@app.route("/hering")
 def hello_world():
-    return "<p> HEY, MY NAME IS DOCKER! </p>"
+    return "<p> Fellipe Fernandes and Felipe Ferraz! </p>"
 
-@app.route("/python")
+@app.route("/arcellor")
 def who_am_i():
-    return "<h1> HEEEEEEEEEEEEEEEEEEEEEY, DO YOU ALREADY KNOW ME?? </h1>"
+    return "<h1> Jonathan e Ketlen </h1>"
 
-@app.route("/friends")
+@app.route("/portobello")
 def get_users():
-    return "<h1> Adilton, Andre, Diego, Marconi, Kayo e Plabo </h1>"
+    return "<h1> Magnum, Lucas e Luan </h1>"
